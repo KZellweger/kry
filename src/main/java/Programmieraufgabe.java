716f682c3c801f0d518ad1ebc76e206140f00193
Kevin Zellweger <kevin.zellweger@students.fhnw.ch>
2022-03-14 10:33:32 +0100

    read inputfile

---
+++ src/main/java/Programmieraufgabe.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+
+public class Programmieraufgabe {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        System.out.println(new String(readChiffre()));
+    }
+
+    private static byte[] readChiffre() throws IOException, URISyntaxException {
+        URL resource = Programmieraufgabe.class.getClassLoader().getResource("chiffre.txt");
+        File chiffre = new File(resource.toURI());
+        return Files.readAllBytes(chiffre.toPath());
+    }
+
+}
--- src/main/java/impl/kevin/SpnImpl.java
+++ src/main/java/impl/kevin/SpnImpl.java
@@ -1,4 +1,46 @@
 package impl.kevin;
 
-public class SpnImpl {
+import symmetric.mode.Mode;
+import symmetric.spn.SPN;
+
+import java.util.Optional;
+
+public class SpnImpl implements SPN {
+
+
+
+    @Override
+    public byte[] decrypt(byte[] encrypted) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] encrypt(byte[] message) {
+        return new byte[0];
+    }
+
+    @Override
+    public void setN(int n) {
+
+    }
+
+    @Override
+    public void setM(int m) {
+
+    }
+
+    @Override
+    public void setR(int r) {
+
+    }
+
+    @Override
+    public void setS(int s) {
+
+    }
+
+    @Override
+    public void useMode(Optional<Mode> mode) {
+
+    }
 }
